2275ba3ba7b598c862eb54df15a21f864df1eba6
Felipe Tsubota <88908355+ftsubota@users.noreply.github.com>
2021-10-29 20:38:33 -0300

    Add files via upload

---
+++ get.php
@@ -0,0 +1,9 @@
+<?php
+//print_r($_POST);
+
+$nome = $_GET['nome'];
+$senha = $_GET['senha'];
+$email = $_GET['email'];
+
+echo "Nome: $nome, Senha: $senha e Email: $email";
+
---
+++ post.php
@@ -0,0 +1,8 @@
+<?php
+//print_r($_post);
+
+$nome = $_POST['nome'];
+$senha = $_POST['senha'];
+$email = $_POST['email'];
+
+echo "Nome: $nome, Senha:$senha e Email: $email";
